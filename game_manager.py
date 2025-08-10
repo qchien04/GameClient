8e21c8e445e7c9adffcc87c21eab23b199a5cee0
Ta Quang Chien <chien1472k4@gmail.com>
2025-08-10 10:05:40 +0700

    o

--- game_manager.py
+++ game_manager.py
@@ -294,18 +294,14 @@ class GameManager:
 
         with self.udp_client.lock:
             if self.udp_client.game_state and "players" in self.udp_client.game_state:
-                state = self.udp_client.game_state["players"].get(Config.PLAYERID)
-                if state and state["new"]==True:
-                    self.udp_client.game_state["players"][Config.PLAYERID]["new"]=False
-                    self.player.set_position(state["x"], state["y"])
-
                     # if state["action"] == Action.SHOOT and state["target_x"] >0 and state["target_y"] > 0:
                     #     self.audio_manager.play_shot()
                     #     self.bullet_manager.shoot((state["x"]+self.player.width//2, state["y"]+self.player.height//2), (state["target_x"]+self.target.width//2, state["target_y"]+self.target.height//2), self.game_state.kill_count)
-                state = self.udp_client.game_state["players"].get(Config.ENEMYID)
-                if state and state["new"]==True:
-                    self.udp_client.game_state["players"][Config.ENEMYID]["new"]=False
-                    self.enemy.set_position(state["x"], state["y"])
+                for player_id in self.udp_client.game_state["players"]:
+                    state = self.udp_client.game_state["players"].get(player_id)
+                    if state and state["new"]==True:
+                        self.udp_client.game_state["players"][player_id]["new"]=False
+                        self.player.set_position(state["x"], state["y"])
 
             if self.udp_client.game_state and "bullets" in self.udp_client.game_state:
                 bullets = self.udp_client.game_state["bullets"]
